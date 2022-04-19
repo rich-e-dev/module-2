fa514c0dfaa20f4a79c62f3a81304955c8e317f2
rich-e-dev <78890842+rich-e-dev@users.noreply.github.com>
2022-04-18 21:51:14 -0700

    Add files via upload

---
+++ Starter_Code 2/loan_qualifier_app/qualifier/utils/fileio.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+
+def save_csv(csv_path, qualifying_loans):
+    """Saves the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+    Returns:
+       Saves the list of lists that contains the rows of data to the CSV file.
+    """
+    with open(csv_path, "w") as csvfile:
+
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+        csvwriter.writerows(qualifying_loans)    
